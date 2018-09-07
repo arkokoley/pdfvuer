35b7ed7103b1c3f9590ab848a31644871bcb8802
Gaurav Koley <arkokoley@live.in>
2018-09-07 13:28:34 +0530

    fix js path error

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: '/pdfvuer/dist/',
     filename: 'build.js'
   },
   module: {
