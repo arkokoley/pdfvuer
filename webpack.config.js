4ff3231efd411515419c02b641a49c982e2ecd69
ramanangru <ramanangru@yahoo.com>
2019-08-01 15:06:20 +0530

    Update webpack.config.js
    This change will help in adding the plugin for dynamic import in the vue project

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,12 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        options: {
+            plugins: [
+                "@babel/plugin-syntax-dynamic-import"
+            ]
+        }
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
